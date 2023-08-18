5c42e0a11c8dd37eb24307824d83538e89202c06
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2023-08-18 18:49:32 +0800

    PETA

---
+++ Comp_1st_PETA.py
@@ -0,0 +1,18 @@
+# Personal info
+
+name = "Jake Zhang"
+bday = "December 05"
+age = "16"
+school = "Philippine Cultural College"
+address = "Tondo, Manila, Philippines"
+contact = "09912209837"
+email = "zhang.jake.valentin.t@philippineculturalcollege.edu.ph"
+
+print("Student Information")
+print("Student's Name:", name)
+print("Birthday:", bday)
+print("Age:", age)
+print("School:", school)
+print("Home Address:", address)
+print("Contact:", contact)
+print("Email:", email)
