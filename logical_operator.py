4de57934fe5c08df2037b735b78ae49bf1c9cbc0
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2023-10-10 18:26:14 +0800

    new file:   logical_operator.py

---
+++ logical_operator.py
@@ -0,0 +1,8 @@
+# Get the input from the user``
+number = int(input("Type an integer: "))
+
+# Determine if the number is divisible by both 2 and 3
+if number % 2 == 0 and number % 3 == 0:
+    print(f"The number {number} is divisible by both 2 and 3.")
+else:
+    print(f"The number {number} is not divisible by both 2 and 3.")
