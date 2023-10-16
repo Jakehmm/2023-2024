9c6bf6973dcf93024025845f7cf180930452b747
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2023-10-16 09:42:04 +0800

    new file:   operator_1.py

---
+++ operator_1.py
@@ -0,0 +1,20 @@
+print(0b11011001+0b00101010)
+print(bin(259))
+print()
+x = 12
+y = 4
+print((x & y) | y)
+print(~(x<<y)&x)
+print((x^2)|(y&x))
+print()
+j = 2
+k = 2
+print(j is k)
+print()
+a = 1
+b = [1,2,3]
+print(a in b)
+print()
+s = "H&D Information Technology"
+y = (1,2,3,4)
+print("&H" in s)
