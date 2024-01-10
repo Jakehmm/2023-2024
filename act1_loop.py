fba378d4a9b442df4f649a6200133b45aedca8e3
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2024-01-10 19:55:00 +0800

    new file:   act1_loop.py

---
+++ act1_loop.py
@@ -0,0 +1,21 @@
+# Zhang, Jake Jiekai
+# G10A
+"""
+@@@@@@@
+   @
+   @
+   @
+@  @
+ @@
+
+"""
+
+for i in range(6):
+    if i == 0:
+        print("@@@@@@@")
+    elif i == 4:
+        print("@  @")
+    elif i == 5:
+        print(" @@")
+    else:
+        print("   @")
