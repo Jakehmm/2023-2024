93cf51a534e9bc16d5e186ce4fef7bebb1b9efde
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2024-01-11 21:58:10 +0800

    modified:   act1_loop.py

--- act1_loop.py
+++ act1_loop.py
@@ -1,26 +1,14 @@
 # Zhang, Jake Jiekai
 # G10A
-"""
-Sample output(letter J):
-@@@@@@@
-   @
-   @
-   @
-@  @
- @@
-"""
 
-# 6 rows
-for row in range(6):
-    # First row
-    if row == 0:
-        print("@@@@@@@")
-    # Second to the last row
-    elif row == 4:
-        print("@  @")
-    # Last row
-    elif row == 5:
-        print(" @@")
-    # Other rows
-    else:
-        print("   @")
+# Big letter j using symbols(@)
+letter_j = ["@@@@@@@",
+            "   @   ",
+            "   @   ",
+            "   @   ",
+            "@  @   ",
+            " @@    "]
+
+# Print every row of letter_j
+for row in letter_j:
+    print(row)
