8fd1cd2bcf9562c0f1220e90f3dcda5d9e44c6a1
Jakehmm <113662115+Jakehmm@users.noreply.github.com>
2024-01-17 23:10:46 +0800

    Add code for computing "Area of Sector."

--- 3rd_PETA2.py
+++ 3rd_PETA2.py
@@ -9,30 +9,38 @@ import sys
 while True:
     print('''Options:
 1. Arc Length
-2. Lens Equation
-3. Sum of Numbers
-4. Exit''')
+2. Area of Sector
+3. Lens Equation
+4. Sum of Numbers
+5. Exit''')
     option = input("Enter the number of your choice: ").strip()
 
     # Compute for arc length in cm
     if option == '1':
         x = int(input("Enter the radius(cm): "))
-        y = int(input("Enter the angle in degrees: "))
+        y = int(input("Enter the central angle in degrees: "))
         print(f"Arc Length: {(x*math.pi) * (y/180)}cm")
 
-    # Compute for image distance in cm
+    # Compute for area of sector in cm^2
     elif option == '2':
+        x = int(input("Enter the radius(cm): "))
+        y = int(input("Enter the central angle in degrees: "))
+        print(f"Area of Sector: {(x*x*math.pi) * (y/360)}cm^2")
+
+    # Compute for image distance in cm
+    elif option == '3':
         x = int(input("Enter the Focal Length(cm): "))
         y = int(input("Enter the Object Distance(cm): "))
 
         # If the object is at the focal length, no image is formed
-        if x == y:
+        if y == x:
             print("Image Distance: No image is formed.")
         else:
-            print(f"Image Distance: {1 / ((1/x) - (1/y))}cm (The answer might not be precise due to the \"floating point imprecision\" of computers)")
+            print(f"Image Distance: {1 / ((1/x) - (1/y))}cm")
+            print("(The answer might not be precise due to the \"floating point imprecision\" of computers)")
 
     # Compute for sum of numbers up to x
-    elif option == '3':
+    elif option == '4':
         x = int(input("Enter a positive integer: "))
         y = 0
 
@@ -43,12 +51,14 @@ while True:
         print(f"Sum of Numbers: {y}")
 
     # Exit the program
-    elif option == '4':
+    elif option == '5':
         sys.exit("Thank you for using this program!")
 
     # Let the user to type again
     else:
         print("Invalid option. Please try again.")
 
-    # Print a new line to separate the result and the options of next loop
+    # Print a new line to separate the result of current loop 
+    # and the options of next loop
     print()
+    
